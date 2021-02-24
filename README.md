880506e172133bb1aa3f438bec23baf275248469
ZacharyNGreen <zacharynathangreen@gmail.com>
2021-02-23 21:41:27 -0500

    adding smart contracts

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # Smart_Contract
 Smart Contract with Solidity Repo
+
+
+#DeferredEquityPlan
+
+DeferredEquityPlan is utilized to institute a deferred equity plan which is common to many companys. THe program is set up to distributed 1000 shares over 4 years to the employee. The contract works by ensuring that the employee receives an initial allocation of 1000 shares with 250 being payable out each year. The contract requires that the employee is eligible for distribution by ensuring tha tthey have 1) passed the eligible time for distribution and 2) still have deferred shares available for distribution
+
+#TieredProfitSplitter
+TieredProfitSplitter is a smart contract that calculates a tiered profit spltis among employees, with the balance going to the CEO. It keeps track of the running total of profit which has been distributed to each employee according to their share of the split and then distributes the remaining balance to the CEO.
+
+#AssociateProfitSplitter
+
+This is a simple contract which is used to evenly distribute the profits equally among employees. 
---
+++ Starter-Code/DeferredEquityPlan.sol
@@ -0,0 +1,64 @@
+pragma solidity ^0.5.0;
+
+// lvl 3: equity plan
+contract DeferredEquityPlan {
+    address human_resources;
+
+    address payable employee; // bob
+    bool active = true; // this employee is active at the start of the contract
+
+    // @TODO: Set the total shares and annual distribution
+    
+    uint shares = 1000;
+    uint annual_distrp=250;
+
+    uint start_time = now; // permanently store the time this contract was initialized
+
+    // @TODO: Set the `unlock_time` to be 365 days from now
+    uint unlock_time=365;
+
+    uint public distributed_shares; // starts at 0
+    distributed_shares=0;
+
+    constructor(address payable _employee) public {
+        human_resources = msg.sender;
+        employee = _employee;
+    }
+
+    function distribute() public {
+        require(msg.sender == human_resources || msg.sender == employee, "You are not authorized to execute this contract.");
+        require(active == true, "Contract not active.");
+
+        // @TODO: Add "require" statements to enforce that:
+        // 1: `unlock_time` is less than or equal to `now`
+        // 2: `distributed_shares` is less than the `total_shares`
+        require(unlock_time<=now && distributed_shares<=total_shares, "have not reached unlock time or have distributed all of your shares");
+        
+
+        // @TODO: Add 365 days to the `unlock_time`
+        unlock_time+=365;
+        
+
+        // @TODO: Calculate the shares distributed by using the function (now - start_time) / 365 days * the annual distribution
+        // Make sure to include the parenthesis around (now - start_time) to get accurate results!
+        // Your code here!
+        distributed_shares=(now - start_time) / 365 days * the annual distribution
+
+
+
+        if (distributed_shares > 1000) {
+            distributed_shares = 1000;
+        }
+    }
+
+    // human_resources and the employee can deactivate this contract at-will
+    function deactivate() public {
+        require(msg.sender == human_resources || msg.sender == employee, "You are not authorized to deactivate this contract.");
+        active = false;
+    }
+
+    // Since we do not need to handle Ether in this contract, revert any Ether sent to the contract directly
+    function() external payable {
+        revert("Do not send Ether to this contract!");
+    }
+}
